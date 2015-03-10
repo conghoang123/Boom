ce27a6a1df4b64f965f709c56dd22b493b471706
conghoang123 <namlunoy@gmail.com>
2015-03-11 01:09:57 +0700

    khoi tao project

---
+++ Assets/Scripts/Ctl_Start.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ctl_Start : MonoBehaviour
+{
+    PhotonView photonView;
+    void Start()
+    {
+        photonView = GetComponent<PhotonView>();
+    }
+    public void Click_VaoNhanh()
+    {
+        //Application.LoadLevel("ready");
+        
+    }
+}
